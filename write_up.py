a2a98d399fc6fb4bd6a307ce00fbcc96d2494b93
Eric Hambro <eric.hambro@prowler.io>
2019-11-05 01:10:18 +0000

    start write up document with jupytext

---
+++ write_up.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: venv
+#     language: python
+#     name: venv
+# ---
+
+# %% [markdown]
+# # LVMoji
+# *an exploration of latent variable models with emojis*
+
+# ## TOC
+# 1. Intro to Gaussian Processes
+# 2. From PCA to the GPLVM
+# 3. Variational Inference with Bayesian GPLVM
+
+# %%
+print('yey')
+
+# %%
